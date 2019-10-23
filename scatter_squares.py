be33bfe63e5ef10a343c027e9ea5d935d1a65dab
Rodrigo <rodrigobdias@gmail.com>
2019-10-23 12:13:36 -0300

    Valores elevado ao cubo

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1,1001))
+x_values = list(range(1,5001))
 
-y_values = [x**2  for x in x_values]
+y_values = [x**3  for x in x_values]
 
 plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=8)
 
@@ -15,7 +15,7 @@ plt.ylabel("Square of Value", fontsize=14)
 plt.tick_params(axis='both', which='major', labelsize=14)
 
 #Define o intervalo para cada eixo
-plt.axis([0, 1100, 0, 1100000])
+plt.axis([0, 5100, 0, 130000000000])
 
 plt.savefig('squares_plot.png', bbox_inches='tight')
 
