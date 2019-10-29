d73329e30073c7bdf2ebb507d3048e7c9e9aed06
Rodrigo <rodrigobdias@gmail.com>
2019-10-29 15:12:46 -0300

    Criando a classe 'Die'

---
+++ die.py
@@ -0,0 +1,14 @@
+from random import randint
+
+class Die():
+    """Uma classe que representa um unico dadp."""
+
+    def __init__(self, num_sides=6):
+        """Supõe que seja uma dado de seis lados."""
+        self._num_sides = num_sides
+
+    def roll(self):
+        """Devolve um valor aleatório entre 1 e o número de lados."""
+        return randint(1, self._num_sides)
+
+    
