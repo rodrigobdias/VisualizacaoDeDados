22b7bb37b62eeb8b8f2696b60540664868c28fdc
Rodrigo <rodrigobdias@gmail.com>
2019-10-30 22:34:50 -0300

    Exibindo os cabeçalhos e suas posições

--- highs_lows.py
+++ highs_lows.py
@@ -5,4 +5,6 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_now = next(reader)
-    print(header_now)
+
+    for index, column_header in enumerate(header_now):
+        print(index, column_header)
